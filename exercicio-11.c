db01105bc2d75902d524f107792429c9eccb9fc3
Soares007 <soaresstar947@gmail.com>
2023-05-30 19:43:43 -0300

    Exercício 11

---
+++ exercicio-11.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int valores[5];
+    int i, contador = 0;
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &valores[i]);
+        if (valores[i] % 2 == 0) {
+            contador++;
+        }
+    }
+    printf("%d valores pares\n", contador);
+    return 0;
+}
